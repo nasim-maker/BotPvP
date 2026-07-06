50cce27736bbff5aab9ce7b04b9807233c3344e8
nasim-maker <oumaramel9@gmail.com>
2026-07-06 23:54:01 +0100

    Update TrainingBot.java

--- src/main/java/com/example/examplemod/bot/TrainingBot.java
+++ src/main/java/com/example/examplemod/bot/TrainingBot.java
@@ -2,22 +2,36 @@ package com.example.examplemod.bot;
 
 public class TrainingBot {
 
-    private String name;
-
-    public TrainingBot() {
-        this.name = "Training Bot";
-        System.out.println("TrainingBot created!");
-    }
+    private String name = "TrainingBot";
+    private int health = 100;
+    private boolean spawned = false;
 
     public void spawn() {
+        spawned = true;
         System.out.println(name + " spawned!");
+    }
+
+    public void attack() {
+        System.out.println(name + " attacks!");
+    }
+
+    public void useShield() {
+        System.out.println(name + " blocks!");
+    }
+
+    public void useMace() {
+        System.out.println(name + " uses Mace!");
     }
 
     public String getName() {
         return name;
     }
 
-    public void setName(String newName) {
-        this.name = newName;
+    public int getHealth() {
+        return health;
+    }
+
+    public boolean isSpawned() {
+        return spawned;
     }
 }
