3e9e53572fed042180fe47ad3c42e6a6d0aded3d
nasim-maker <oumaramel9@gmail.com>
2026-07-06 23:29:09 +0100

    Update TrainingBot.java

--- src/main/java/com/example/examplemod/bot/TrainingBot.java
+++ src/main/java/com/example/examplemod/bot/TrainingBot.java
@@ -1,13 +1,23 @@
-
 package com.example.examplemod.bot;
 
 public class TrainingBot {
 
+    private String name;
+
     public TrainingBot() {
+        this.name = "Training Bot";
         System.out.println("TrainingBot created!");
     }
 
     public void spawn() {
-        System.out.println("Bot spawned!");
+        System.out.println(name + " spawned!");
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        this.name = newName;
     }
 }
